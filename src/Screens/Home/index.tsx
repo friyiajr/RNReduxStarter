d3dcc29d7466d63c38a78603ed762ef7f05e5687
Daniel Friyia <dfriyia@gmail.com>
2022-04-15 14:41:27 -0400

    [DF] chore: add navigation

---
+++ src/Screens/Home/index.tsx
@@ -0,0 +1,42 @@
+import {useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React from 'react';
+
+import {View, Text, StyleSheet, Button} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {incrementIndex} from '../../Redux/Pokemon/Pokemon.reducer';
+import Pokemon from '../Pokemon';
+
+const Home = () => {
+  const nav = useNavigation<StackNavigationProp<any>>();
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch(incrementIndex());
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+      <Button
+        title="Go To Next"
+        onPress={() => {
+          // nav.push(Pokemon.routeName);
+          increment();
+        }}
+      />
+    </View>
+  );
+};
+
+Home.routeName = 'Home';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Home;
